e2e88d16175e01d3402b8f7ae533c226a4df70e5
WillDang14 <will.dang1402@gmail.com>
2025-01-05 11:59:53 -0500

    user auth and favorite

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,12 +3,25 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    mainController,
-    getRecipes,
-    getRecipeById,
-    searchByIngredient,
+  mainController,
+  getRecipes,
+  getRecipeById,
+  searchByIngredient,
 } = require("../controllers/mainController.js");
-const { route } = require("../app.js");
+
+//
+const { register } = require("../controllers/registerController");
+const { login } = require("../controllers/loginController.js");
+const { logout } = require("../controllers/logoutController.js");
+
+const authenticate = require("../middleware/authenticate");
+
+//
+const {
+  getAllFavorite,
+  createFavorite,
+  deleteFavorite,
+} = require("../controllers/favoriteController.js");
 
 /* ============================================================= */
 router.get("/", mainController.get);
@@ -18,5 +31,16 @@ router.get("/recipes", getRecipes);
 router.get("/recipes/:id", getRecipeById);
 
 router.get("/search", searchByIngredient);
+
+//
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+
+//
+router.get("/favorite", authenticate, getAllFavorite);
+router.post("/favorite", authenticate, createFavorite);
+router.delete("/favorite", authenticate, deleteFavorite);
+
 /* ============================================================= */
 module.exports = router;
