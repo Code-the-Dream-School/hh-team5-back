e5bcd9518a652342f7811656d9def482ad6156d9
WillDang14 <will.dang1402@gmail.com>
2025-01-20 20:34:00 -0500

    add Sing's list

--- src/app.js
+++ src/app.js
@@ -14,7 +14,15 @@ const cookieParser = require("cookie-parser");
 // const cookieParser = require("cookie-parser");
 /* ============================================================= */
 // middleware
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
